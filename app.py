3af0c492ddf7535828b0e660485c70f812cf6159
Zuzanna Brzęczek <95574391+zbrzeczek@users.noreply.github.com>
2024-01-19 23:28:14 +0100

    zmienne

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,5 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
